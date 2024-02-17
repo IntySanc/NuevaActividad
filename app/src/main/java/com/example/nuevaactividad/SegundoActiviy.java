5fee9a2667f615f6505ba54acadf421a9cfeb79f
Inty_Sanc <inty_sanchez.gra@hotmail.com>
2024-02-17 14:34:06 -0600

    Tarea Sesion 5

--- app/src/main/java/com/example/nuevaactividad/SegundoActiviy.java
+++ app/src/main/java/com/example/nuevaactividad/SegundoActiviy.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
     public class SegundoActiviy extends AppCompatActivity {
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segundo_activiy);
@@ -14,5 +14,6 @@ import android.view.View;
     public void Anterior(View view) {
         Intent anterior = new Intent(this, MainActivity.class);
         startActivity(anterior);
+
     }
-}
+    }
